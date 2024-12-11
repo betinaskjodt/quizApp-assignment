f2b1e8aab5d082b97aec21a69488528f3a9e83ce
betinaskjodt <betina_horthe@hotmail.com>
2024-12-11 22:50:36 +0100

    added Readme File

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,62 @@
+const questions = [
+  {
+    question: "What day is halloween?",
+    answers: ["31.October", "1.November", "13.October"],
+    correctAnswer: "31. october",
+    userAnswer: "",
+  },
+  {
+    question: "Where do real vampirebats live?",
+    answers: [
+      "Both in North and South America",
+      "South America",
+      "North America",
+    ],
+    correctAnswer: "Both in North and South America",
+    userAnswer: "",
+  },
+  {
+    question: "What was Dr. Frankenstein's first name?",
+    answers: ["Henrik", "Victor", "Alexander"],
+    correctAnswer: "Victor",
+    userAnswer: "",
+  },
+  {
+    question:
+      "Which country celebrates 'Day of the Dead' instead of Halloween?",
+    answers: ["China", "Spain", "Mexico"],
+    correctAnswer: "Mexico",
+    userAnswer: "",
+  },
+  {
+    question: "Which orange fruit is widely used during Halloween?",
+    answers: ["Orange", "Pumpkin", "Persimmon"],
+    correctAnswer: "Pumpkin",
+    userAnswer: "",
+  },
+  {
+    question: "Which animal is associated with Halloween?",
+    answers: ["Black snakes", "Black dogs", "Black cats"],
+    correctAnswer: "Black cats",
+    userAnswer: "",
+  },
+  {
+    question: "When did Halloween celebrations in Norway make a breakthrough?",
+    answers: [
+      "The end of the 19th century",
+      "In the early 2000s",
+      "Around the 1980s",
+    ],
+    correctAnswer: "In the early 2000s",
+    userAnswer: "",
+  },
+  {
+    question: "What country did the real Dracula rule over?",
+    answers: ["Luxembourg", "Austria", "Romania"],
+    correctAnswer: "Romania",
+    userAnswer: "",
+  },
+];
 
 const quizContainer = document.querySelector(".quiz__container");
 const startButton = document.querySelector(".quiz__start-btn");
@@ -5,229 +64,174 @@ const questionContainer = document.querySelector(".quiz__question-container");
 const quizQuestion = document.querySelector(".quiz__question-text");
 const quizOptionContainer = document.querySelector(".quiz__question-options");
 const nextButton = document.querySelector(".quiz__next-btn");
-// const quizProgressContainer = document.querySelector(".quiz__progress-container");
+const progressContainer = document.querySelector(".quiz__progress-container");
 const progressText = document.querySelector(".quiz__progress-text");
 const restartButton = document.querySelector(".quiz__restart-btn");
 const reviewButton = document.querySelector(".quiz__review-btn");
-const reviewContainer =document.querySelector(".quiz__review-container");
-
-const questions = [
-    {
-        question: "1. What day is halloween?",
-        answers: ["31. october", "1. november", "13. october"],
-        correctAnswer : "31. october",
-        userAnswer: ""
-    },
-    {
-        question: "2. Where do real vampirebats live?",
-        answers: ["Both in North and South America", "South America", "North America"],
-        correctAnswer: "Both in North and South America",
-        userAnswer: ""
-    },
-    {
-        question: "3 What was Dr. Frankenstein's first name?", 
-        answers: ["Henrik", "Victor", "Alexander"],
-        correctAnswer: "Victor",
-        userAnswer: ""
-    },
-    {
-        question: "4 Which country celebrates 'Day of the Dead' instead of Halloween?", 
-        answers: ["China", "Spain", "Mexico"],
-        correctAnswer: "Mexico",
-        userAnswer: ""
-    },
-    {
-        question: "5 Which orange fruit is widely used during Halloween?", 
-        answers: ["Orange", "Pumpkin", "Persimmon"],
-        correctAnswer: "Pumpkin",
-        userAnswer: ""
-    },
-    {
-        question: "6 Which animal is associated with Halloween?", 
-        answers: ["Black snakes", "Black dogs", "Black cats"],
-        correctAnswer: "Black cats",
-        userAnswer: ""
-    },
-    {
-        question: "7 When did Halloween celebrations in Norway make a breakthrough?", 
-        answers: ["The end of the 19th century", "In the early 2000s", "Around the 1980s"],
-        correctAnswer: "In the early 2000s",
-        userAnswer: ""
-    },
-    {
-        question: "8 What country did the real Dracula rule over?", 
-        answers: ["Luxembourg", "Austria", "Romania"],
-        correctAnswer: "Romania",
-        userAnswer: ""
-    },
-];
+const reviewContainer = document.querySelector(".quiz__review-container");
 
 let progressscore = 0;
 let currentQuestion = 0;
 
-const startQuiz = ()=>{
-    startButton.classList.add("hide");
-    progressscore = 0;
-    currentQuestion = 0;
-    showQuestion();
+const startQuiz = () => {
+  nextButton.textContent = "Next";
+  reviewButton.classList.add("hide");
+  reviewContainer.classList.add("hide");
+  progressContainer.classList.add("hide");
+
+  progressscore = 0;
+  currentQuestion = 0;
+
+  startButton.classList.add("hide");
+  questionContainer.classList.remove("hide");
+  nextButton.classList.remove("hide");
+  restartButton.classList.remove("hide");
+
+  showQuestion();
 };
 
-const showQuestion = ()=>{
-    questionContainer.classList.remove("hide");
-    restartButton.classList.remove("hide");
+const showQuestion = () => {
+  progressContainer.classList.remove("hide");
+  quizOptionContainer.textContent = "";
+  const question = questions[currentQuestion];
+  quizQuestion.textContent = question.question;
 
-    const question = questions[currentQuestion];
-    quizQuestion.textContent = question.question;
+  const questionOptionButtons = [];
 
-    quizOptionContainer.innerHTML = "";
+  question.answers.forEach((answer) => {
+    const questionOption = document.createElement("button");
+    questionOption.classList.add("quiz__question-options-btn");
+    questionOption.textContent = answer;
+    questionOptionButtons.push(questionOption);
 
-    // quizQuestionContainer.innerHTML = "";
+    questionOption.addEventListener("click", (e) => {
+      checkAnswer(answer, questionOptionButtons);
 
-    // quizQuestion.textContent = questions[currentQuestion].question;
-
-    // if(currentQuestion < questions.length){
-        
-    //     currentQuestion++;
-    // } else {
-    //     quizQuestionContainer.classList.add("hide");
-    //     reviewButton.classList.remove("hide");
-    // }
-
-        question.answers.forEach(answer =>{
-            const questionOption = document.createElement("button");
-            questionOption.classList.add("quiz__question-options-btn");
-            questionOption.textContent = answer;
-            questionOption.addEventListener("click",  ()=>{
-                checkAnswer(answer);
-            });
-            // questionOption.addEventListener("mouseenter", ()=> {
-            //     questionOption.style.backgroundColor = "black";
-            // })
-            quizOptionContainer.appendChild(questionOption);
-        });
-    // questions.forEach(question =>{
-
-    //     question.answers.forEach( (answer)=>{
-    //         const questionOption = document.createElement("button");
-    //         questionOption.classList.add("quiz__question-options-btn");
-    //         quizQuestionOptions.append(questionOption);
-    //         // console.log(`answer ${index +1}: ${answer}`);
-    //         questionOption.textContent = answer;
-    //         questionOption.addEventListener("click", ()=>{
-
-    //         })
-    //         // for(let answer in element){
-
-    //         // }
-    //     });
-    // });
-    showProgressBar();
-};
-
-const checkAnswer = (selectedAnswer)=>{
-    questions[currentQuestion].userAnswer = selectedAnswer;
-
-    if(selectedAnswer === questions[currentQuestion].correctAnswer){
-        progressscore++
-    }; 
-    nextButton.disabled = false
-    // currentQuestion++;
-    // if(currentQuestion >= questions.length){
-    //     showScore();
-    //     // too soon will not show next question?
-    // } else {
-    //     showQuestion();
-    // }
-};
-
-const nextQuestion =()=>{
-    quizOptionContainer.innerHTML = "";
-};
-
-const showProgressBar = ()=>{
-    progressText.textContent = `question ${currentQuestion +1} of ${questions.length}`;
-
-};
-
-const resetQuiz = ()=>{
-    currentQuestion = 0;
-    progressscore = 0;
-    showQuestion()
-};
-
-const showScore = ()=>{
-    questionContainer.classList.add("hide");
-    restartButton.classList.remove("hide");
-    reviewButton.classList.remove("hide");
-};
-
-const reviewResult = ()=>{
-    const result = document.createElement("p");
-    result.textContent = `Final Score: ${progressscore} out of ${questions.length} 😎`;
-    // reviewContainer.append(result);
-    // result.classList.add("result");
-    // quizContainer.append(result);
-    // const resultheader = document.createElement("h2");
-    // const resultParagraph = document.createElement("p");
-    // resultheader.textContent = "Your Result from Quiz 😎";
-    // resultParagraph.textContent = `Final Score: ${progressscore} out of ${questions.length} 😎`;
-    // result.append(resultheader, resultParagraph);
-    reviewContainer.appendChild(result)
-    questions.forEach(question => {
-        const questionReviewContainer = document.createElement("div");
-        const questionReviewText = document.createElement("p");
-        const questionReviewUserAnswer = document.createElement("p");
-        reviewContainer.append(questionReviewContainer);
-        questionReviewContainer.append(questionReviewText, questionReviewUserAnswer);
-        
-        questionReviewText.textContent = question.question;
-        questionReviewUserAnswer.textContent = `The correct answer is ${question.correctAnswer} you picked ${question.userAnswer}`;
+      e.target.classList.add("quiz__question-options-btn--active");
     });
 
+    quizOptionContainer.appendChild(questionOption);
+  });
+
+  showProgressBar();
 };
 
-const restartQuiz = ()=>{
-    startButton.classList.remove("hide");
-    questionContainer.classList.add("hide");
-    restartButton.classList.add("hide");
-    reviewButton.classList.add("hide");
+const checkAnswer = (selectedAnswer, questionOptionButtons) => {
+  questions[currentQuestion].userAnswer = selectedAnswer;
 
-    reviewContainer.textContent = "";
-    progressText.textContent = "";
-
-    currentQuestion = 0;
-    progressscore = 0;
+  if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+    progressscore++;
+  }
+  questionOptionButtons.forEach((btn) => {
+    btn.disabled = true;
+  });
+  nextButton.disabled = false;
 };
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    questionContainer.classList.add("hide");
-    restartButton.classList.add("hide");
-    reviewButton.classList.add("hide");
+const showProgressBar = () => {
+  progressText.textContent = `Question ${currentQuestion + 1} of ${
+    questions.length
+  }`;
+};
 
+const showScore = () => {
+  questionContainer.classList.add("hide");
+  restartButton.classList.remove("hide");
+  reviewButton.classList.remove("hide");
+  nextButton.classList.add("hide");
+
+  quizQuestion.textContent = "";
+  quizOptionContainer.textContent = "";
+
+  progressText.textContent = `Final score: ${progressscore} out of ${questions.length} 😎`;
+};
+
+const reviewResult = () => {
+  quizContainer.classList.remove("hide");
+  progressContainer.classList.add("hide");
+  progressText.classList.add("hide");
+  questionContainer.classList.add("hide");
+  reviewButton.classList.add("hide");
+  reviewContainer.classList.remove("hide");
+
+  if (!quizQuestion.textContent.trim()) {
+    quizQuestion.classList.add("hide");
+  }
+
+  if (!quizOptionContainer.textContent.trim()) {
+    quizOptionContainer.classList.add("hide");
+  }
+
+  reviewContainer.textContent = "";
+
+  const result = document.createElement("h2");
+  result.classList.add("result");
+  result.textContent = `Final Score: ${progressscore} out of ${questions.length} 😎`;
+  reviewContainer.appendChild(result);
+
+  questions.forEach((question) => {
+    const questionReviewContainer = document.createElement("div");
+    questionReviewContainer.classList.add("review-container");
+
+    const questionReviewText = document.createElement("p");
+    const questionReviewUserAnswer = document.createElement("p");
+    const questionCorrectAnswer = document.createElement("p");
+
+    reviewContainer.appendChild(questionReviewContainer);
+    questionReviewContainer.append(
+      questionReviewText,
+      questionReviewUserAnswer,
+      questionCorrectAnswer
+    );
+
+    questionReviewText.textContent = question.question;
+    questionReviewUserAnswer.textContent = `- You answered ${question.userAnswer}.`;
+    questionCorrectAnswer.textContent = `- The correct answer is ${question.correctAnswer}.`;
+  });
+};
+
+const restartQuiz = () => {
+  startButton.classList.remove("hide");
+  questionContainer.classList.add("hide");
+  nextButton.classList.add("hide");
+  restartButton.classList.add("hide");
+  reviewButton.classList.add("hide");
+  reviewContainer.classList.add("hide");
+
+  quizQuestion.textContent = "";
+  quizOptionContainer.textContent = "";
+  reviewContainer.textContent = "";
+  progressText.textContent = "";
+
+  currentQuestion = 0;
+  progressscore = 0;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  restartButton.classList.add("hide");
+  reviewButton.classList.add("hide");
+  nextButton.classList.add("hide");
+  reviewContainer.classList.add("hide");
 });
 
 startButton.addEventListener("click", startQuiz);
 
-nextButton.addEventListener("click", ()=>{
-    currentQuestion++;
-    nextButton.disabled = true;
-    
-    if(currentQuestion < questions.length){
-        showQuestion()
-    
-        if (currentQuestion === questions.length -1 ) {
-            nextButton.textContent = "Submit"
-        }
-    } else {
-        progressText.textContent = "";
-        showScore()
-    };
+nextButton.addEventListener("click", () => {
+  currentQuestion++;
+  nextButton.disabled = true;
+
+  if (currentQuestion < questions.length) {
+    showQuestion();
+
+    if (currentQuestion === questions.length - 1) {
+      nextButton.textContent = "Submit";
+    }
+  } else {
+    progressText.textContent = "";
+    showScore();
+  }
 });
 
 restartButton.addEventListener("click", restartQuiz);
 
 reviewButton.addEventListener("click", reviewResult);
-
-
-
-
---
+++ src/readme.md
@@ -0,0 +1,46 @@
+# Quiz App
+
+A halloween inspired quiz that works for big and small screens
+
+## Description
+
+In this project the task was to make an quiz app, I chose to make halloween inspired quiz.
+By making this quiz I got to practice and learning more on how to display different content and using JavaScript to build interation when user clicks thought the 8 questions and reviw the result after.
+
+## Instalation
+
+to install the project localy follow these steps:
+
+clone the repoistory:
+git clone https://github.com/betinaskjodt/quizApp-assignment.git
+Navigate into the project directory:
+
+cd quizApp-assignment
+open the index.html in your web browser to get stated.
+
+- no build tools are required for this project, since its a front-end project with html,css and javascript.
+
+## Usage
+
+to use this app:
+
+- open the index.html file in your web browser. click on "start Quiz".
+- Answer the quiestions by clicking on one of the alternatives.
+- click on next to go to the nex question.
+- when you are on the last question submit.
+- If you want to review your result you can by clicking on "review" button.
+- whenever in the progress of the quiz you can restart it by clicking on "restart" button.
+
+## Features
+
+- answering the 8 questions by clicking on the alternativ answers.
+- review your answer at the when submiting.
+- Restart the game whenever in the quiz.
+
+## License
+
+This project is licensed under the MIT License - see the License file for details.
+
+## Contact
+
+If you have any questions or sugetions, reach out to me at [betina_horthe@hotmail.com].
